e6573ae9fa9a143c098abf416bcebd32fe84da42
Mujahid Ali <itxmujahidali@gmail.com>
2021-08-09 05:28:44 -0700

    Auto Primary Key add in the database!

--- crudAPP/models.py
+++ crudAPP/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class WebUser(models.Model):
 
+    id = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=21)
     lastName = models.CharField(max_length=20)
     fatherName = models.CharField(max_length=50)
